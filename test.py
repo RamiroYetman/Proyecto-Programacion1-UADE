073ccbfd002fd2145c410f9dab5572710155ec9c
Ramiro Yetman <ramiroyetman@hotmail.com>
2024-09-24 21:58:33 -0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-def eliminar (diccionario):
-    cont = 0
-    for claves in diccionario.keys():
-        diccionario.pop(claves)
-        cont += 1
-    return diccionario,cont
-cliente = {"nombre" : "Ramiro", "apellido" : "Yetman", "edad": "22"}
-resultado, contado = eliminar(cliente)
-print (resultado, contado)
